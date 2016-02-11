72c88005174fc990fc4ecf001973fd00556be427
cogliaWho <coglia@gmail.com>
2016-02-11 17:22:56 +0100

    updated page costruzioni

--- application/views/pages/costruzione.php
+++ application/views/pages/costruzione.php
@@ -8,17 +8,100 @@
 	</div>
 	<div class="container-half height-auto">
 			<?php echo $text; ?>
-		<div class="vert-third img-list">
-			<img class="full-img" alt="" src="resources/images/costruzione/1.jpg">
+		<div class="vert-half list-macchine">
+			<h2>Fresatura a Controllo Numerico</h2>
+			<ul>
+				<li><span>1</span> fresa Parpas Famu 915 PHS 5 assi alta velocit&agrave;</li>
+				<li><span>2</span> frese Parpas  Famu 812 PHS 3 assi alta velocit&agrave;</li>
+				<li><span>1</span> fresa Parpas OMV 1300 BFC 4 assi</li>
+				<li><span>1</span> fresa Parpas Famu FAS-1-M 3 assi</li>
+				<li><span>1</span> fresa Comu  CLV 800 VL 3 assi</li>
+				<li><span>1</span> fresa Comu MB 1200 GL 3 assi</li>
+				<li><span>1</span> fresa CME FS-1/A 3 assi PLC-530</li>
+			</ul>
+
+			<h2>Fresatura manuale visualizzata</h2>
+			<ul>
+				<li><span>1</span> fresa Imduma 1.000 x 500 mm</li>
+			</ul>
+
+			<h2>Foratura</h2>
+			<ul>
+				<li><span>1</span> foratrice IMSA per forature profonde MF 1000 AF 	&empty;3-	&empty;25.</li>
+			</ul>
+
+			<h2>Tornitura a Controllo Numerico</h2>
+			<ul>
+				<li><span>1</span> tornio CMT Temis 205 2 assi 12 cambi utensile PLC-GE-FANUC-Series-21i-T</li>
+			</ul>
+
+			<h2>Tornitura manuale visualizzata</h2>
+			<ul>
+				<li><span>1</span> tornio OMG 260</li>
+			</ul>
+
+			<h2>Erosione a filo a Controllo Numerico</h2>
+			<ul>
+				<li><span>1</span> Erosione Filo ROBOCUT-a-1iB 4 assi PLC-FANUC-Series-180iS-WB</li>
+			</ul>
+
+			<h2>Erosione a tuffo a Controllo Numerico</h2>
+			<ul>
+				<li><span>1</span> Erosione Tuffo AEG ELOTHERM 332 BI 4 assi 6 cambi elettrodo PLC-ISO</li>
+			</ul>
+
+			<h2>Erosione a Tuffo manuale visualizzata</h2>
+			<ul>
+				<li><span>1</span> AEG ELOPLUS VDI 3400</li>
+				<li><span>1</span> Techosprtk A500</li>
+			</ul>
+
+			<h2>Rettifica tangenziale visualizzata</h2>
+			<ul>
+				<li><span>1</span> rettificatrice Camut maxi 820x2000</li>
+				<li><span>1</span> rettificatrice Freepoin Pittori SGS-1020 AHD</li>
+			</ul>
+
+			<h2>Trapano radiale</h2>
+			<ul>
+				<li><span>1</span> LTF 40X1250</li>
+			</ul>
+
+			<h2>Trapano fresa a colonna con tavola a croce</h2>
+			<ul>
+				<li><span>1</span> Famup</li>
+			</ul>
+
+			<h2>Seghetto nastro</h2>
+			<ul>
+				<li><span>1</span> BS 400SHI IMET</li>
+				<li><span>1</span> THOMAS MOD 255</li>
+			</ul>
+
+			<h2>Magazzino Modula Automatico per utensili e attrezzature</h2>
+			<ul>
+				<li><span>1</span> Modula Sintes 1.3</li>
+			</ul>
+
+			<h2>Metrologia</h2>
+			<ul>
+				<li><span>1</span> Tesa Micro –Hite 3D</li>
+			</ul>
+
 		</div>
-		<div class="vert-third img-list">
-			<img class="full-img" alt="" src="resources/images/costruzione/2.jpg">
-		</div>
-		<div class="vert-third img-list">
-			<img class="full-img" alt="" src="resources/images/costruzione/3.jpg">
-		</div>
-		<div class="vert-third img-list">
-			<img class="full-img" alt="" src="resources/images/costruzione/4.jpg">
+		<div class="vert-half">
+			<div class="vert-full img-list">
+				<img class="full-img" alt="" src="resources/images/costruzione/1.jpg">
+			</div>
+			<div class="vert-full img-list">
+				<img class="full-img" alt="" src="resources/images/costruzione/2.jpg">
+			</div>
+			<div class="vert-full img-list">
+				<img class="full-img" alt="" src="resources/images/costruzione/3.jpg">
+			</div>
+			<div class="vert-full img-list">
+				<img class="full-img" alt="" src="resources/images/costruzione/4.jpg">
+			</div>
 		</div>
 		<div class="clear-divs"></div>
 	</div>
--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,31 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('browserify', function(){
 	browserify('./dev/js/main.js')
 		.bundle()
 		.pipe(source('main.js'))
-		.pipe(gulp.dest('resources/js'));	
+		.pipe(gulp.dest('resources/js'));
 });
 
 gulp.task('copy', function(){
 	gulp.src('dev/js/**/*.*')
 		.pipe(gulp.dest('resources/js'));
 	gulp.src('dev/images/**/*.*')
-		.pipe(gulp.dest('resources/images'));	
+		.pipe(gulp.dest('resources/images'));
 });
 
 gulp.task('styles', function() {
     gulp.src('dev/sass/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('resources/css'));
+        .pipe(gulp.dest('resources/css'))
+        .pipe(livereload());
 });
 
 gulp.task('default', ['browserify', 'copy', 'styles'], function(){
+  livereload.listen();
 	gulp.watch('dev/sass/**/*.scss',['styles']);
 	return gulp.watch('dev/**/*.*', ['browserify', 'copy']);
 });
